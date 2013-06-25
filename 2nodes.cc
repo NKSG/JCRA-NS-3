51eaf3b699b9d2cc1ac7eb9b8c99b7516450474d
bitcpf <bitcpf@gmail.com>
2013-06-25 15:42:59 -0500

    Add 2 band, need to test 4 nodes running

--- 2nodes.cc
+++ 2nodes.cc
@@ -17,11 +17,13 @@
  *
  * Author: Pengfei Cui
  * Date: June 19, 2013
+ * 
  * Description: 
  *	
  *	Step1: Verify the Friis channel model can set frequency
- *	
- *	Notes:
+ *	Step2: Set up 2 band at the same time
+ *
+ * Notes:
  *	Config::SetDefault( ) can change the frequency
  *	YanswifiChannelHelper.AddPropagationLossModel could set the propagation loss model and config the frequency
  *
@@ -30,9 +32,15 @@
  *
  *
  *	In process
- *
+ *		Build networks have 2 bands
  *		Support multiband simultaneous
  *		Output data properly
+ *
+ *	Status:
+ *
+ *		Modify the experiment function to access 2 channel propagation wavelength
+ *
+ *  
  */
 
 #include "ns3/core-module.h"
@@ -48,196 +56,218 @@ NS_LOG_COMPONENT_DEFINE ("Main");
 
 using namespace ns3;
 
+
 class Experiment
 {
-public:
-  Experiment ();
-  Experiment (std::string name);
-  Gnuplot2dDataset Run (const WifiHelper &wifi, const YansWifiPhyHelper &wifiPhy,
-                        const NqosWifiMacHelper &wifiMac, const YansWifiChannelHelper &wifiChannel);
-private:
-  void ReceivePacket (Ptr<Socket> socket);
-  void SetPosition (Ptr<Node> node, Vector position);
-  Vector GetPosition (Ptr<Node> node);
-  void AdvancePosition (Ptr<Node> node);
-  Ptr<Socket> SetupPacketReceive (Ptr<Node> node);
+	public:
+		Experiment ();
+		Experiment (std::string name);
+		Gnuplot2dDataset Run (const WifiHelper &wifi, const YansWifiPhyHelper &wifiPhy,
+				const NqosWifiMacHelper &wifiMac, const YansWifiChannelHelper &wifichannel2, const YansWifiChannelHelper &wifiChannel5);
+	private:
+		void ReceivePacket (Ptr<Socket> socket);
+		void SetPosition (Ptr<Node> node, Vector position);
+		Vector GetPosition (Ptr<Node> node);
+		void AdvancePosition (Ptr<Node> node);
+		Ptr<Socket> SetupPacketReceive (Ptr<Node> node);
 
-  uint32_t m_bytesTotal;
-  Gnuplot2dDataset m_output;
+		uint32_t m_bytesTotal;
+		Gnuplot2dDataset m_output;
 };
+
+
+
 
 Experiment::Experiment ()
 {
 }
 
-Experiment::Experiment (std::string name)
-  : m_output (name)
+
+
+
+
+	Experiment::Experiment (std::string name)
+: m_output (name)
 {
-  m_output.SetStyle (Gnuplot2dDataset::LINES);
+	m_output.SetStyle (Gnuplot2dDataset::LINES);
 }
 
-void
+
+
+	void
 Experiment::SetPosition (Ptr<Node> node, Vector position)
 {
-  Ptr<MobilityModel> mobility = node->GetObject<MobilityModel> ();
-  mobility->SetPosition (position);
+	Ptr<MobilityModel> mobility = node->GetObject<MobilityModel> ();
+	mobility->SetPosition (position);
 }
+/*
+   Vector
+   Experiment::GetPosition (Ptr<Node> node)
+   {
+   Ptr<MobilityModel> mobility = node->GetObject<MobilityModel> ();
+   return mobility->GetPosition ();
+   }
 
-Vector
-Experiment::GetPosition (Ptr<Node> node)
-{
-  Ptr<MobilityModel> mobility = node->GetObject<MobilityModel> ();
-  return mobility->GetPosition ();
-}
-
-void 
-Experiment::AdvancePosition (Ptr<Node> node) 
-{
-  Vector pos = GetPosition (node);
-  double mbs = ((m_bytesTotal * 8.0) / 1000000);
-  m_bytesTotal = 0;
-  m_output.Add (pos.x, mbs);
-  pos.x += 60.0;
-  if (pos.x >= 2210.0) 
-    {
-      return;
-    }
-  SetPosition (node, pos);
-  //std::cout << "x="<<pos.x << std::endl;
-  Simulator::Schedule (Seconds (1.0), &Experiment::AdvancePosition, this, node);
+   void 
+   Experiment::AdvancePosition (Ptr<Node> node) 
+   {
+   Vector pos = GetPosition (node);
+   double mbs = ((m_bytesTotal * 8.0) / 1000000);
+   m_bytesTotal = 0;
+   m_output.Add (pos.x, mbs);
+   pos.x += 60.0;
+   if (pos.x >= 2210.0) 
+   {
+   return;
+   }
+   SetPosition (node, pos);
+//std::cout << "x="<<pos.x << std::endl;
+Simulator::Schedule (Seconds (1.0), &Experiment::AdvancePosition, this, node);
 }
 
 void
 Experiment::ReceivePacket (Ptr<Socket> socket)
 {
-  Ptr<Packet> packet;
-  while ((packet = socket->Recv ()))
-    {
-      m_bytesTotal += packet->GetSize ();
-    }
+Ptr<Packet> packet;
+while ((packet = socket->Recv ()))
+{
+m_bytesTotal += packet->GetSize ();
+}
 }
 
 Ptr<Socket>
 Experiment::SetupPacketReceive (Ptr<Node> node)
 {
-  TypeId tid = TypeId::LookupByName ("ns3::PacketSocketFactory");
-  Ptr<Socket> sink = Socket::CreateSocket (node, tid);
-  sink->Bind ();
-  sink->SetRecvCallback (MakeCallback (&Experiment::ReceivePacket, this));
-  return sink;
+TypeId tid = TypeId::LookupByName ("ns3::PacketSocketFactory");
+Ptr<Socket> sink = Socket::CreateSocket (node, tid);
+sink->Bind ();
+sink->SetRecvCallback (MakeCallback (&Experiment::ReceivePacket, this));
+return sink;
 }
+
+
+*/
+
+
 
 Gnuplot2dDataset
 Experiment::Run (const WifiHelper &wifi, const YansWifiPhyHelper &wifiPhy,
-                 const NqosWifiMacHelper &wifiMac, const YansWifiChannelHelper &wifiChannel)
+		const NqosWifiMacHelper &wifiMac, const YansWifiChannelHelper &wifiChannel2, const YansWifiChannelHelper &wifiChannel5)
 {
-  m_bytesTotal = 0;
+/*	m_bytesTotal = 0;
 
-  NodeContainer c;
-  c.Create (2);
+	NodeContainer c;
+	c.Create (2);
 
-  PacketSocketHelper packetSocket;
-  packetSocket.Install (c);
+	PacketSocketHelper packetSocket;
+	packetSocket.Install (c);
 
-  YansWifiPhyHelper phy = wifiPhy;
-  phy.SetChannel (wifiChannel.Create ());
+	YansWifiPhyHelper phy = wifiPhy;
+	phy.SetChannel (wifiChannel.Create ());
 
-  NqosWifiMacHelper mac = wifiMac;
-  NetDeviceContainer devices = wifi.Install (phy, mac, c);
+	NqosWifiMacHelper mac = wifiMac;
+	NetDeviceContainer devices = wifi.Install (phy, mac, c);
 
-  MobilityHelper mobility;
-  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-  positionAlloc->Add (Vector (0.0, 0.0, 0.0));
-  positionAlloc->Add (Vector (100.0, 0.0, 0.0));
-  mobility.SetPositionAllocator (positionAlloc);
-  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+	MobilityHelper mobility;
+	Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
+	positionAlloc->Add (Vector (0.0, 0.0, 0.0));
+	positionAlloc->Add (Vector (100.0, 0.0, 0.0));
+	mobility.SetPositionAllocator (positionAlloc);
+	mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
 
-  mobility.Install (c);
+	mobility.Install (c);
 
-  PacketSocketAddress socket;
-  socket.SetSingleDevice (devices.Get (0)->GetIfIndex ());
-  socket.SetPhysicalAddress (devices.Get (1)->GetAddress ());
-  socket.SetProtocol (1);
+	PacketSocketAddress socket;
+	socket.SetSingleDevice (devices.Get (0)->GetIfIndex ());
+	socket.SetPhysicalAddress (devices.Get (1)->GetAddress ());
+	socket.SetProtocol (1);
 
-  OnOffHelper onoff ("ns3::PacketSocketFactory", Address (socket));
-  onoff.SetConstantRate (DataRate (60000000));
-  onoff.SetAttribute ("PacketSize", UintegerValue (2000));
+	OnOffHelper onoff ("ns3::PacketSocketFactory", Address (socket));
+	onoff.SetConstantRate (DataRate (60000000));
+	onoff.SetAttribute ("PacketSize", UintegerValue (2000));
 
-  ApplicationContainer apps = onoff.Install (c.Get (0));
-  apps.Start (Seconds (0.5));
-  apps.Stop (Seconds (10.0));
+	ApplicationContainer apps = onoff.Install (c.Get (0));
+	apps.Start (Seconds (0.5));
+	apps.Stop (Seconds (10.0));
 
-  Simulator::Schedule (Seconds (1.5), &Experiment::AdvancePosition, this, c.Get (1));
-  Ptr<Socket> recvSink = SetupPacketReceive (c.Get (1));
+	Simulator::Schedule (Seconds (1.5), &Experiment::AdvancePosition, this, c.Get (1));
+	Ptr<Socket> recvSink = SetupPacketReceive (c.Get (1));
 
-  Simulator::Run ();
+	Simulator::Run ();
 
-  Simulator::Destroy ();
+	Simulator::Destroy ();
 
-  return m_output;
+	*/
+	return m_output;
+
 }
 
 int main (int argc, char *argv[])
 {
-  // disable fragmentation
-  Config::SetDefault ("ns3::WifiRemoteStationManager::FragmentationThreshold", StringValue ("2200"));
-  Config::SetDefault ("ns3::WifiRemoteStationManager::RtsCtsThreshold", StringValue ("2200"));
+	// disable fragmentation
+	Config::SetDefault ("ns3::WifiRemoteStationManager::FragmentationThreshold", StringValue ("2200"));
+	Config::SetDefault ("ns3::WifiRemoteStationManager::RtsCtsThreshold", StringValue ("2200"));
 
-//  Config::SetDefault ("ns3::FriisPropagationLossModel::Lambda",DoubleValue(0.025));
-//  Config::SetDefault ("ns3::FriisPropagationLossModel::SystemLoss",DoubleValue(1.0));
-
-
-
-  CommandLine cmd;
-  cmd.Parse (argc, argv);
-
-  Gnuplot gnuplot = Gnuplot ("24Ghz.png");
-
-  Experiment experiment;
-  WifiHelper wifi = WifiHelper::Default ();
-//  wifi.SetStandard (WIFI_PHY_STANDARD_80211a);
-  NqosWifiMacHelper wifiMac = NqosWifiMacHelper::Default ();
-  YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
-  YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
-
-
-  wifiChannel.AddPropagationLoss("ns3::FriisPropagationLossModel","Lambda",DoubleValue(0.025));
-  Gnuplot2dDataset dataset;
-
-  wifiMac.SetType ("ns3::AdhocWifiMac");
-
-  NS_LOG_DEBUG ("54");
-  experiment = Experiment ("54mb");
-  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
-                                "DataMode", StringValue ("OfdmRate54Mbps"));
-  dataset = experiment.Run (wifi, wifiPhy, wifiMac, wifiChannel);
-  std::cout << "New log from here bitcpfbitcpf " << std::endl; 
-  gnuplot.AddDataset (dataset);
-
- 
-/* 
-  Config::SetDefault ("ns3::FriisPropagationLossModel::Lambda",DoubleValue(0.125));
-  Config::SetDefault ("ns3::FriisPropagationLossModel::SystemLoss",DoubleValue(1.0));
-  
-  wifiChannel.AddPropagationLoss("ns3::FriisPropagationLossModel");
-
-  wifiMac.SetType ("ns3::AdhocWifiMac");
-  
-  
-  NS_LOG_DEBUG ("New Wavelength");
-  experiment = Experiment ("48m");
-  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
-                                "DataMode", StringValue ("OfdmRate54Mbps"));
-  dataset = experiment.Run (wifi, wifiPhy, wifiMac, wifiChannel);
-  gnuplot.AddDataset (dataset);
-*/
-  std::ofstream asc;
-  asc.open("testj18.txt");
-  gnuplot.GenerateOutput (asc);
+	//  Config::SetDefault ("ns3::FriisPropagationLossModel::Lambda",DoubleValue(0.025));
+	//  Config::SetDefault ("ns3::FriisPropagationLossModel::SystemLoss",DoubleValue(1.0));
 
 
 
-//  gnuplot.GenerateOutput (std::cout);
+	CommandLine cmd;
+	cmd.Parse (argc, argv);
 
-  return 0;
+	Gnuplot gnuplot = Gnuplot ("24Ghz.png");
+
+	Experiment experiment;
+	WifiHelper wifi = WifiHelper::Default ();
+	//  wifi.SetStandard (WIFI_PHY_STANDARD_80211a);
+	NqosWifiMacHelper wifiMac = NqosWifiMacHelper::Default ();
+	YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
+	YansWifiChannelHelper wifiChannel2 = YansWifiChannelHelper::Default ();
+	YansWifiChannelHelper wifiChannel5 = YansWifiChannelHelper::Default ();
+
+	// Add Propagation to the channel models
+	wifiChannel5.AddPropagationLoss("ns3::FriisPropagationLossModel","Lambda",DoubleValue(0.025));
+	wifiChannel2.AddPropagationLoss("ns3::FriisPropagationLossModel","Lambda",DoubleValue(0.125));
+
+
+	// Add output dataset
+	Gnuplot2dDataset dataset;
+
+	wifiMac.SetType ("ns3::AdhocWifiMac");
+
+
+	NS_LOG_DEBUG ("54");
+	experiment = Experiment ("Test");
+
+	// Set wifi parameters
+	wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+			"DataMode", StringValue ("OfdmRate54Mbps"));
+	dataset = experiment.Run (wifi, wifiPhy, wifiMac, wifiChannel2,wifiChannel5);
+	//  std::cout << "New log from here bitcpfbitcpf " << std::endl; 
+	//  gnuplot.AddDataset (dataset);
+
+
+	/* 
+	   Config::SetDefault ("ns3::FriisPropagationLossModel::Lambda",DoubleValue(0.125));
+	   Config::SetDefault ("ns3::FriisPropagationLossModel::SystemLoss",DoubleValue(1.0));
+
+	   wifiChannel.AddPropagationLoss("ns3::FriisPropagationLossModel");
+
+	   wifiMac.SetType ("ns3::AdhocWifiMac");
+
+
+	   NS_LOG_DEBUG ("New Wavelength");
+	   experiment = Experiment ("48m");
+	   wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+	   "DataMode", StringValue ("OfdmRate54Mbps"));
+	   dataset = experiment.Run (wifi, wifiPhy, wifiMac, wifiChannel);
+	   gnuplot.AddDataset (dataset);
+	   */
+
+
+
+	//  gnuplot.GenerateOutput (std::cout);
+
+	return 0;
 }
